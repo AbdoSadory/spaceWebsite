11044aad5a63ad638fddfe9c4ea3e1ccb3799522
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2023-04-25 14:35:24 +0200

    package json edit.

--- src/App.js
+++ src/App.js
@@ -1,83 +1,86 @@
-import "./App.css";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Header from "./component/header/Header";
-import Home from "./pages/home/Home";
-import Destination from "./pages/destination/Destination";
-import Crew from "./pages/crew/Crew";
-import Technology from "./pages/technology/Technology";
-import { useEffect, useState } from "react";
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './component/header/Header'
+import Home from './pages/home/Home'
+import Destination from './pages/destination/Destination'
+import Crew from './pages/crew/Crew'
+import Technology from './pages/technology/Technology'
+import { useEffect, useState } from 'react'
+import NotFound from './component/notFound/NotFound'
 
 function App() {
-  let [apiData, setapiData] = useState([]);
-  let [error, setError] = useState(false);
-  let [isLoading, setIsLoading] = useState(true);
-  let url = useLocation();
+  let [apiData, setapiData] = useState([])
+  let [error, setError] = useState(false)
+  let [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
-    const fetchController = new AbortController();
+    const fetchController = new AbortController()
 
-    fetch("https://abdosadory.github.io/spaceWebsite/data/data.json", {
+    fetch('https://abdosadory.github.io/spaceWebsite/data/data.json', {
       signal: fetchController.signal,
     })
       .then((res) => {
         if (!res.ok) {
-          throw new Error("couldn't fetch");
+          throw new Error("couldn't fetch")
         }
-        return res.json();
+        return res.json()
       })
       .then((data) => {
-        setapiData(data);
-        setIsLoading(false);
+        setapiData(data)
+        setIsLoading(false)
       })
       .catch((err) => {
-        setError(true);
-        setIsLoading(false);
-        if (err.name === "AbortError") {
-          console.log("fetching is aborted");
+        setError(true)
+        setIsLoading(false)
+        if (err.name === 'AbortError') {
+          console.log('fetching is aborted')
         }
-        console.log(err.message);
-      });
-    return () => fetchController.abort();
-  }, []);
+        console.log(err.message)
+      })
+    return () => fetchController.abort()
+  }, [])
   return (
     <>
       <Header />
       <main>
-        <Routes>
-          <Route path="/spaceWebsite" element={<Home />} exact />
-          <Route
-            path="/destination"
-            element={
-              <Destination
-                destinations={apiData && apiData["destinations"]}
-                loading={isLoading}
-                error={error}
-              />
-            }
-          />
-          <Route
-            path="/crew"
-            element={
-              <Crew
-                crew={apiData && apiData["crew"]}
-                loading={isLoading}
-                error={error}
-              />
-            }
-          />
-          <Route
-            path="/technology"
-            element={
-              <Technology
-                technology={apiData && apiData["technology"]}
-                loading={isLoading}
-                error={error}
-              />
-            }
-          />
-        </Routes>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/spaceWebsite" element={<Home />} />
+            <Route
+              path="/destination"
+              element={
+                <Destination
+                  destinations={apiData && apiData['destinations']}
+                  loading={isLoading}
+                  error={error}
+                />
+              }
+            />
+            <Route
+              path="/crew"
+              element={
+                <Crew
+                  crew={apiData && apiData['crew']}
+                  loading={isLoading}
+                  error={error}
+                />
+              }
+            />
+            <Route
+              path="/technology"
+              element={
+                <Technology
+                  technology={apiData && apiData['technology']}
+                  loading={isLoading}
+                  error={error}
+                />
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.js";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+import './index.css'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
